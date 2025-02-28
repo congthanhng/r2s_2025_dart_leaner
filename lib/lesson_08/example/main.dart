a4ef3130f68742b8bbd23492617aec67609e1794
Kong <15138747+congthanhng@users.noreply.github.com>
2025-02-28 18:49:45 +0700

    lesson 07

---
+++ lib/lesson_08/example/main.dart
@@ -0,0 +1,257 @@
+import 'dart:developer';
+
+import 'package:flutter/material.dart';
+
+import 'route_named.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  runApp(const MyApp());
+}
+
+//O nam hinh Login co 1 textField nhap name.
+//Nham quay ai thi hien thi welcom $name
+// Sau khi mo man hinh Home thi hien thi text: Welcom $name to Flutter
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: OnBoardPage(),
+      // routes: {
+      //   RouteNamed.onBoardPage: (context) => const OnBoardPage(),
+      //   RouteNamed.homePage: (context)=> const HomePage(),
+      //   RouteNamed.loginPage: (context)=> const LoginPage(),
+      // },
+      onGenerateRoute: (settings) {
+        log('Setting');
+        switch (settings.name) {
+          case RouteNamed.onBoardPage:
+            return MaterialPageRoute(
+              builder: (context) => OnBoardPage(),
+            );
+
+          case RouteNamed.homePage:
+            final data = settings.arguments as Map<String, dynamic>;
+            final name = data['nameKey'] as String;
+            return MaterialPageRoute(
+              builder: (context) => HomePage(
+                name: name,
+              ),
+            );
+
+          case RouteNamed.loginPage:
+            return MaterialPageRoute(
+              builder: (context) => LoginPage(),
+            );
+
+          default:
+            return MaterialPageRoute(
+              builder: (context) => PageNotFound(),
+            );
+        }
+      },
+    );
+  }
+}
+
+class PageNotFound extends StatelessWidget {
+  const PageNotFound({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        color: Colors.blue,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          spacing: 30,
+          children: [
+            Center(child: Text('404 Page noy FOund')),
+            ElevatedButton(
+                onPressed: () {
+                  Navigator.pushReplacementNamed(context, RouteNamed.homePage);
+                },
+                child: Text('Home'))
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+//red
+class OnBoardPage extends StatefulWidget {
+  const OnBoardPage({super.key});
+
+  @override
+  State<OnBoardPage> createState() => _OnBoardPageState();
+}
+
+class _OnBoardPageState extends State<OnBoardPage> {
+  String nameFromLogin = '';
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        color: Colors.white,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          spacing: 30,
+          children: [
+            Center(child: Text('Welcome $nameFromLogin')),
+            ElevatedButton(
+                onPressed: () async {
+                  bool? result = true;
+                  if (nameFromLogin.isEmpty) {
+                    await showModalBottomSheet(
+                      context: context,
+                      isDismissible: false,
+                      builder: (context) {
+                        return Container(
+                          // height: 400,
+                            color: Colors.red,
+                            child: Center(
+                              child: Text('Ban chua nhap ten, ban co muon tiep tuc'),
+                            ));
+                      },
+                    );
+                    // result = await showDialog<bool>(
+                    //   context: context,
+                    //   barrierDismissible: false,
+                    //   builder: (context) {
+                    //     return SimpleDialog(
+                    //       // initialTime: TimeOfDay(hour: 15, minute: 20),
+                    //       // firstDate: DateTime.now(),
+                    //       // lastDate: DateTime.now().add(Duration(days: 15)),
+                    //
+                    //       title: Text('Ban chua nhap ten'),
+                    //       children: [
+                    //         TextButton(
+                    //             onPressed: () {
+                    //               Navigator.pop(context, false);
+                    //             },
+                    //             child: Text('Cancel')),
+                    //         TextButton(
+                    //             onPressed: () {
+                    //               Navigator.pop(context, true);
+                    //             },
+                    //             child: Text('OK')),
+                    //       ],
+                    //       // content:
+                    //       //     Text('Ban chua nhap ten, ban co muon tiep tuc'),
+                    //       // actions: [
+                    //       //   TextButton(
+                    //       //       onPressed: () {
+                    //       //         Navigator.pop(context, false);
+                    //       //       },
+                    //       //       child: Text('Cancel')),
+                    //       //   TextButton(
+                    //       //       onPressed: () {
+                    //       //         Navigator.pop(context, true);
+                    //       //       },
+                    //       //       child: Text('OK')),
+                    //       // ],
+                    //     );
+                    //   },
+                    // );
+                  }
+
+                  // if (result ?? false) {
+                  //   Navigator.pushNamed(context, RouteNamed.homePage,
+                  //       arguments: <String, dynamic>{
+                  //         'nameKey': nameFromLogin,
+                  //       });
+                  // }
+                },
+                child: Text('Go Home')),
+            ElevatedButton(
+                onPressed: () async {
+                  final result =
+                      await Navigator.pushNamed(context, RouteNamed.loginPage);
+                  if (result is String && result.isNotEmpty) {
+                    nameFromLogin = result;
+                    setState(() {});
+                  }
+                },
+                child: Text('Go Login'))
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+//blue
+class HomePage extends StatelessWidget {
+  const HomePage({super.key, this.name});
+
+  final String? name;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        color: Colors.blue,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          spacing: 30,
+          children: [
+            Center(child: Text('Welcom $name to Flutter')),
+            ElevatedButton(
+                onPressed: () {
+                  Navigator.pushNamed(context, RouteNamed.loginPage);
+                },
+                child: Text('Go Login'))
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class LoginPage extends StatelessWidget {
+  const LoginPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    String name = '';
+    return Scaffold(
+      body: Container(
+        color: Colors.green,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          spacing: 30,
+          children: [
+            Center(child: Text('LoginPage')),
+            TextFormField(
+              onChanged: (value) {
+                name = value;
+              },
+              decoration: InputDecoration(
+                  border: OutlineInputBorder(), hintText: 'Input your Name'),
+            ),
+            ElevatedButton(
+                onPressed: () {
+                  if (Navigator.canPop(context)) {
+                    Navigator.pop(context, name);
+                  }
+                },
+                child: Text('Back'))
+          ],
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/lesson_08/example/route_named.dart
@@ -0,0 +1,7 @@
+class RouteNamed {
+  static const onBoardPage = '/onBoardPage';
+  static const homePage = '/homePage';
+  static const loginPage = '/loginPage';
+  static const productDetailPage = '/productDetailPage';
+  // static const pageNotFound = '/';
+}
